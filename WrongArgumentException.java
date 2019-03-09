32f98a193c73225e1e037a4131922485916ebf0d
Rui yin <yaluaiace@gmail.com>
2019-03-09 16:17:07 -0600

    Delete WrongArgumentException.java

--- WrongArgumentException.java
+++
@@ -1,9 +0,0 @@
-/**
- * @author Rui Yin 
- * @date 2019-01-28 13:33
- */
-public class WrongArgumentException extends Exception{
-    public WrongArgumentException(String message) {
-        super(message);
-    }
-}
